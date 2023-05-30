792c0129f6f3b979d65e1a0d6d80135028f895e6
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-30 20:58:47 -0300

    first application in React.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const firstBook = {
+  author: 'Piggyback',
+  title:
+    "The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collector's Edition",
+  image: './images/theLegendOfZelda.jpg',
+}
+
+const secondBook = {
+  author: 'Dr. Seuss',
+  title: "Oh, the Places You'll Go!",
+  image: './images/secondBook.jpg',
+}
+
+const thirdBook = {
+  author: ' James Clear',
+  title: 'Atomic Habits',
+  image: './images/atomicHabits.jpg',
+}
+
+const fourthBook = {
+  author: 'Peter Attia',
+  title: 'Outlive',
+  image: './images/outlive.jpg',
+}
+
+// const title =
+//   "The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collector's Edition"
+// const author = 'Piggyback'
+// const image = './images/zeldaBook.jpg'
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.image}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.image}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.image}
+      />
+      <Book
+        author={fourthBook.author}
+        title={fourthBook.title}
+        img={fourthBook.image}
+      />
+    </section>
+  )
+}
+
+const Book = ({author, title, img}) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
