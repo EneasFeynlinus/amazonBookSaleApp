dc0b616d4c54239204dc0ddf0ea335909ab450fe
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-31 11:23:23 -0300

    Training map to build a list on React.

--- src/index.js
+++ src/index.js
@@ -3,61 +3,36 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-const firstBook = {
-  author: 'Piggyback',
-  title:
-    "The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collector's Edition",
-  image: './images/theLegendOfZelda.jpg',
-}
-
-const secondBook = {
-  author: 'Dr. Seuss',
-  title: "Oh, the Places You'll Go!",
-  image: './images/secondBook.jpg',
-}
-
-const thirdBook = {
-  author: ' James Clear',
-  title: 'Atomic Habits',
-  image: './images/atomicHabits.jpg',
-}
-
-const fourthBook = {
-  author: 'Peter Attia',
-  title: 'Outlive',
-  image: './images/outlive.jpg',
-}
-
-// const title =
-//   "The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collector's Edition"
-// const author = 'Piggyback'
-// const image = './images/zeldaBook.jpg'
+const books = [
+  {
+    author: 'Piggyback',
+    title:
+      "The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collector's Edition",
+    image: './images/theLegendOfZelda.jpg',
+  },
+  
+  {
+    author: 'Dr. Seuss',
+    title: "Oh, the Places You'll Go!",
+    image: './images/secondBook.jpg',
+  },
+  
+  {
+    author: ' James Clear',
+    title: 'Atomic Habits',
+    image: './images/atomicHabits.jpg',
+  },
+  
+  {
+    author: 'Peter Attia',
+    title: 'Outlive',
+    image: './images/outlive.jpg',
+  },
+]
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.image}
-      ></Book>
-
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.image}
-      />
-      <Book
-        author={thirdBook.author}
-        title={thirdBook.title}
-        img={thirdBook.image}
-      />
-      <Book
-        author={fourthBook.author}
-        title={fourthBook.title}
-        img={fourthBook.image}
-      />
-    </section>
+    <section className="booklist">{newEquipments}</section>
   )
 }
 
